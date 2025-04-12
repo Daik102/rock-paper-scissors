547282e4205895a7a28d055293f23b060826b279
Daik102 <fruit@kd6.so-net.ne.jp>
2025-04-12 15:58:06 +0900

    Create three buttons and add an event-listener to the buttons

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -3,11 +3,11 @@ let computerScore = 0;
 
 function getComputerChoice() {
   const randomNum = Math.random();
-  let computerChoice;
+  let computerChoice = '';
   
   if (randomNum <= 1 / 3) {
     computerChoice = "rock";
-  } else if (randomNum > 1 / 3 && randomNum <= 2 / 3) {
+  } else if (randomNum <= 2 / 3) {
     computerChoice = "paper";
   } else {
     computerChoice = "scissors";
@@ -15,61 +15,25 @@ function getComputerChoice() {
   return computerChoice;
 }
 
-function getHumanChoice() {
-  const initialInput = prompt("Enter your choice", "");
-  const humanChoice = initialInput.toLowerCase();
-  return humanChoice;
-}
-
 function playRound(humanChoice, computerChoice) {
   if (humanChoice === computerChoice) {
     console.log("Tie");
-  } else if (humanChoice === "rock") {
-      if (computerChoice === "paper") {
-        youLose();
-      } else if (computerChoice === "scissors") {
-        youWin();
-      }
-  } else if (humanChoice === "paper") {
-      if (computerChoice === "rock") {
-        youWin();
-      } else if (computerChoice === "scissors") {
-        youLose();
-      }
-  } else if (humanChoice === "scissors") {
-      if (computerChoice === "rock") {
-        youLose();
-      } else if (computerChoice === "paper") {
-        youWin();
-      }
+  } else if (humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "paper" && computerChoice === "rock" || humanChoice === "scissors" && computerChoice === "paper") {
+    console.log("You win!");
+    humanScore++;
+  } else {
+    console.log("You lose!");
+    computerScore++;
   }
   console.log(`Your score: ${humanScore}`);
   console.log(`Computer's score: ${computerScore}`);
 }
 
-function youWin() {
-  console.log("You win!");
-  humanScore++;
-}
-
-function youLose() {
-  console.log("You lose!");
-  computerScore++;
-}
-
-let playCount = 0;
-
-function playGame() {
-  if (playCount >= 5) {
-    return;
-  } else {
+const btns = document.querySelectorAll('.btn');
+btns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    const humanSelection = e.target.id;
     const computerSelection = getComputerChoice();
-    const humanSelection = getHumanChoice();
-
     playRound(humanSelection, computerSelection);
-    playCount++;
-    playGame();
-  }
-}
-
-playGame();
+  });
+});
