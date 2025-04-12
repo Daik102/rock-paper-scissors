43d168602ae19d427771fa6e1ae6c40b08e00030
Daik102 <fruit@kd6.so-net.ne.jp>
2025-04-12 20:11:34 +0900

    Finish the project

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 This is a rock paper scissors project for the odin project.
+You came to a dungeon to defeat the evil monster with your rps weapons.
+The world peace depends on you.
+Hope you enjoy!
--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,16 @@
+document.body.style.marginLeft = '20px';
+document.body.style.fontFamily = 'Arial, sans-serif';
+const monster = document.querySelector('.monster');
+const results = document.querySelector('.results');
+const star = document.querySelector('.star');
 let humanScore = 0;
 let computerScore = 0;
-const results = document.querySelector('.results');
+let tie = 0;
+let wins = 0;
+let finished;
+let currentMonster = 'slime';
+
+monster.innerHTML = 'You encountered a slime!';
 
 function getComputerChoice() {
   const randomNum = Math.random();
@@ -19,24 +29,96 @@ function getComputerChoice() {
 function playRound(humanChoice, computerChoice) {
   if (humanChoice === computerChoice) {
     results.textContent = 'Tie';
+    tie++;
   } else if (humanChoice === 'rock' && computerChoice === 'scissors' || humanChoice === 'paper' && computerChoice === 'rock' || humanChoice === 'scissors' && computerChoice === 'paper') {
     results.textContent = 'You win!';
     humanScore++;
   } else {
-    results.textContent = 'You lose!';
+    results.textContent = 'You lose';
     computerScore++;
   }
-  results.innerHTML += '<br>' + `Your score: ${humanScore}` + '<br>' + `Computer's score: ${computerScore}`;
-  if (humanScore === 5) {
-    results.innerHTML += '<br>' + 'Congrats! You won the game!';
-  } else if (computerScore === 5) {
-    results.innerHTML += '<br>' + 'You lost the game';
+  results.innerHTML += '<br>' + `Your score: ${humanScore}` + '<br>' + `Monster's score: ${computerScore}` + '<br>' + `Tie: ${tie}`;
+  if (humanScore === 5 || computerScore === 5) {
+    if (humanScore === 5) {
+      results.innerHTML += '<br>' + `Congrats! You beat the ${currentMonster}!`;
+      star.innerHTML += '&#9734;';
+      wins++;
+      const descendBtn = document.createElement('button');
+      if (wins === 6) {
+        descendBtn.textContent = 'Return';
+      } else {
+        descendBtn.textContent = 'Descend';
+      }
+      document.body.appendChild(descendBtn);
+      descendBtn.addEventListener('click', () => {
+        if (wins === 1) {
+          document.body.style.background = '#ccc';
+          currentMonster = 'Goblin';
+        } else if (wins === 2) {
+          document.body.style.background = '#999';
+          currentMonster = 'Lizard';
+        } else if (wins === 3) {
+          document.body.style.background = '#666';
+          document.body.style.color = '#fff';
+          currentMonster = 'Skelton';
+        } else if (wins === 4) {
+          document.body.style.background = '#333';
+          currentMonster = 'Dark knight';
+        } else if (wins === 5) {
+          document.body.style.background = '#000';
+          currentMonster = 'Dragon';
+        } else if (wins === 6) {
+          document.body.style.background = '#fff';
+          document.body.style.color = '#000';
+          document.body.style.fontSize = '20px';
+          document.body.style.marginTop = '20px';
+          document.body.innerHTML = '&#9734;&#9734;&#9734;&#9734;&#9734;&#9734;&#9734;&#9734;&#9734;&#9734;' + '<br><br>' + 'You saved the world!' + '<br>' + 'Thanks for playing' + '<br><br>' + 'Daik 2025' + '<br><br>' + '&#9734;&#9734;&#9734;&#9734;&#9734;&#9734;&#9734;&#9734;&#9734;&#9734;';
+          return;
+        }
+        finished = false;
+        humanScore = 0;
+        if (wins === 4) {
+          computerScore = 1;
+        } else if (wins === 5) {
+          computerScore = 2;
+        } else {
+          computerScore = 0;
+        }
+        tie = 0;
+        results.textContent = '';
+        document.body.removeChild(descendBtn);
+        monster.textContent = `You encountered a ${currentMonster}!`;
+      });
+    } else {
+      results.innerHTML += '<br>' + `You lost to the ${currentMonster}`;
+      const tryAgainBtn = document.createElement('button');
+      tryAgainBtn.textContent = 'Try Again';
+      document.body.appendChild(tryAgainBtn);
+      tryAgainBtn.addEventListener('click', () => {
+        finished = false;
+        humanScore = 0;
+        if (wins === 4) {
+          computerScore = 1;
+        } else if (wins === 5) {
+          computerScore = 2; 
+        } else {
+          computerScore = 0;
+        }
+        tie = 0;
+        results.textContent = '';
+        document.body.removeChild(tryAgainBtn);
+      });
+    }
+    finished = true;
   }
 }
 
 const btns = document.querySelectorAll('.btn');
 btns.forEach((btn) => {
   btn.addEventListener('click', (e) => {
+    if (finished) {
+      return;
+    }
     const humanSelection = e.target.id;
     const computerSelection = getComputerChoice();
     playRound(humanSelection, computerSelection);
