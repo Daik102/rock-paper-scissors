50bd1bcf54ef07e2c4451a668f46ef13434342dd
Daik102 <fruit@kd6.so-net.ne.jp>
2025-04-12 16:23:02 +0900

    Display result and score and announce a winner

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,32 +1,37 @@
 let humanScore = 0;
 let computerScore = 0;
+const results = document.querySelector('.results');
 
 function getComputerChoice() {
   const randomNum = Math.random();
   let computerChoice = '';
   
   if (randomNum <= 1 / 3) {
-    computerChoice = "rock";
+    computerChoice = 'rock';
   } else if (randomNum <= 2 / 3) {
-    computerChoice = "paper";
+    computerChoice = 'paper';
   } else {
-    computerChoice = "scissors";
+    computerChoice = 'scissors';
   }
   return computerChoice;
 }
 
 function playRound(humanChoice, computerChoice) {
   if (humanChoice === computerChoice) {
-    console.log("Tie");
-  } else if (humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "paper" && computerChoice === "rock" || humanChoice === "scissors" && computerChoice === "paper") {
-    console.log("You win!");
+    results.textContent = 'Tie';
+  } else if (humanChoice === 'rock' && computerChoice === 'scissors' || humanChoice === 'paper' && computerChoice === 'rock' || humanChoice === 'scissors' && computerChoice === 'paper') {
+    results.textContent = 'You win!';
     humanScore++;
   } else {
-    console.log("You lose!");
+    results.textContent = 'You lose!';
     computerScore++;
   }
-  console.log(`Your score: ${humanScore}`);
-  console.log(`Computer's score: ${computerScore}`);
+  results.innerHTML += '<br>' + `Your score: ${humanScore}` + '<br>' + `Computer's score: ${computerScore}`;
+  if (humanScore === 5) {
+    results.innerHTML += '<br>' + 'Congrats! You won the game!';
+  } else if (computerScore === 5) {
+    results.innerHTML += '<br>' + 'You lost the game';
+  }
 }
 
 const btns = document.querySelectorAll('.btn');
